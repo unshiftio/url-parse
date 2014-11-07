564dfebcc988370080ec10e5d6c9406418322753
Arnout Kazemier <info@3rd-Eden.com>
2014-11-07 23:06:01 +0100

    [fix] Remove ports if they are default ports [fix] Generate location every time [fix] Allow pre-defined default location [fix] Allow url.parse / URL interface [fix] lowercase the hostname & protocol [fix] better custom stringify

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var qs = require('querystringify')
-  , loca = require('./location');
+var required = require('requires-port')
+  , lolcation = require('./lolcation')
+  , qs = require('querystringify');
 
 //
-// MOARE: Mother Of All Regular Expressions
+// MOARE: Mother Of All Regular Expressions.
 //
 var regexp = /^(?:(?:(([^:\/#\?]+:)?(?:(?:\/\/)(?:(?:(?:([^:@\/#\?]+)(?:\:([^:@\/#\?]*))?)@)?(([^:\/#\?\]\[]+|\[[^\/\]@#?]+\])(?:\:([0-9]+))?))?)?)?((?:\/?(?:[^\/\?#]+\/+)*)(?:[^\?#]*)))?(\?[^#]+)?)(#.*)?/
   , keys = ',,protocol,username,password,host,hostname,port,pathname,query,hash'.split(',')
@@ -16,34 +17,75 @@ var regexp = /^(?:(?:(([^:\/#\?]+:)?(?:(?:\/\/)(?:(?:(?:([^:@\/#\?]+)(?:\:([^:@\
  * faster and it pleases my CDO.
  *
  * @constructor
- * @param {String} address The address we want to parse.
- * @param {Boolean|function} query Parse the query string.
+ * @param {String} address URL we want to parse.
+ * @param {Boolean|function} parser Parser for the query string.
+ * @param {Object} location Location defaults for relative paths.
  * @api public
  */
-function URL(address, query) {
-  if (!(this instanceof URL)) return new URL(address, query);
+function URL(address, location, parser) {
+  if (!(this instanceof URL)) return new URL(address, location, parser);
 
-  for (var i = 0, bits = regexp.exec(address); i < parts; i++) {
-    if (keys[i]) this[keys[i]] = bits[i] || loca[keys[i]] || '';
+  //
+  // The following if statements allows this module two have compatibility with
+  // 2 different API:
+  //
+  // 1. Node.js's `url.parse` api which accepts a URL, boolean as arguments
+  //    where the boolean indicates that the query string should also be parsed.
+  //
+  // 2. The `URL` interface of the browser which accepts a URL, object as
+  //    arguments. The supplied object will be used as default values / fall-back
+  //    for relative paths.
+  //
+  if ('function' === typeof location) { parser = location; location = null; }
+  if ('object' === typeof parser) { location = parser; parser = null; }
+  if (parser && 'function' !== typeof parser) parser = qs.parse;
+
+  location = lolcation(location);
+
+  for (var i = 0, bits = regexp.exec(address), key; i < parts; key = keys[++i]) {
+    if (key) {
+      this[key] = bits[i] || location[key] || '';
+
+      //
+      // The protocol, host, host name should always be lower cased even if they
+      // are supplied in uppercase. This way, when people generate an `origin`
+      // it be correct.
+      //
+      if (i === 2 || i === 5 || i === 6) this[key] = this[key].toLowerCase();
+    }
   }
 
-  if (query) {
-    if ('function' === typeof query) this.query = query(this.query);
-    else this.query = qs.parse(this.query);
-  }
+  //
+  // Also parse the supplied query string in to an object. If we're supplied
+  // with a custom parser as function use that instead of the default build-in
+  // parser.
+  //
+  if (parser) this.query = parser(this.query);
 
+  //
+  // We should not add port numbers if they are already the default port number
+  // for a given protocol.
+  //
+  if (!required(this.port, this.protocol)) this.port = '';
+
+  //
+  // The href is just the compiled result.
+  //
   this.href = this.toString();
 }
 
 /**
  * Transform the properties back in to a valid and full URL string.
  *
- * @param {Function} query Optional query stringify function.
+ * @param {Function} stringify Optional query stringify function.
  * @returns {String}
  * @api public
  */
-URL.prototype.toString = function toString(query) {
-  var result = this.protocol +'//';
+URL.prototype.toString = function toString(stringify) {
+  if (stringify && 'function' !== typeof stringify) stringify = qs.stringify;
+
+  var result = this.protocol +'//'
+    , query;
 
   if (this.username) result += this.username +':'+ this.password +'@';
 
@@ -53,8 +95,10 @@ URL.prototype.toString = function toString(query) {
   result += this.pathname;
 
   if (this.query) {
-    if ('string' !== typeof this.query) result += '?'+ (query || qs.stringfy)(this.query);
-    else result += this.query;
+    if (stringify) query = stringify(this.query);
+    else query = this.query;
+
+    result += (query.charAt(0) === '?' ? '' : '?') + query;
   }
 
   if (this.hash) result += this.hash;
