da4d8d978e65d477e62695113e0df9ef349bf7f5
Darrin Holst <darrinholst@gmail.com>
2017-09-18 18:11:40 -0500

    Add missing properties to typescript definition

--- index.d.ts
+++ index.d.ts
@@ -12,9 +12,10 @@ declare module "url-parse" {
         new (url: string, baseURL?: string, parseQuery?: boolean|QueryParser): ParsedUrl;
 
         protocol: string;   // protocol: Requested protocol without slashes (e.g. http:).
+        slashes: boolean;   // slashes: Indicates whether the protocol is followed by two forward slashes (//).
+        auth: string;       // auth: Authentication information portion (e.g. username:password).
         username: string;   // username: Username of basic authentication.
         password: string;   // password: Password of basic authentication.
-        auth: string;       // auth: Authentication information portion (e.g. username:password).
         host: string;       // host: Host name with port number.
         hostname: string;   // hostname: Host name without port number.
         port: string;       // port: Optional port number.
@@ -22,6 +23,7 @@ declare module "url-parse" {
         query: Query;       // query: Parsed object containing query string, unless parsing is set to false.
         hash: string;       // hash: The "fragment" portion of the URL including the pound-sign (#).
         href: string;       // href: The full URL.
+        origin: string;     // origin: The origin of the URL.
 
         toString(): string;
         set(key: string, value: string|Object|number): ParsedUrl;
